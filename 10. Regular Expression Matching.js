baac2f2b5b34086b41db8895f143e982c5378670
Daniel Roberts <BeardOfDan@gmail.com>
2018-11-12 03:04:23 -0800

    (feat) Set up logic for "Regular Expression Matching"

--- 10. Regular Expression Matching.js
+++ 10. Regular Expression Matching.js
@@ -4,7 +4,37 @@
  * @return {boolean}
  */
 var isMatch = function (s, p) {
+  const patterns = [];
 
+  for (let i = 0; i < p.length; i++) {
+    if (i + 1 < p.length) {
+      const next = p[i + 1];
+      if ((next === '*') || (next === '.')) {
+        patterns.push(p[i] + next);
+        i++; // skip the next char
+        continue; // skip the single char literal case
+      }
+    } // else just a character literal match
+    patterns.push(p[i]);
+  }
+
+  let match = true;
+  let patternsIndex = 0;
+  for (let i = 0; i < s.length; i++) {
+    const pattern = patterns[patternsIndex];
+    if (pattern.length > 1) {
+      if (pattern[1] === '*') {
+
+      } else { // pattern[1] === '.'
+
+      }
+    } else { // single character literal
+
+    }
+  }
+
+
+  return match;
 };
 
 // Specifications:
