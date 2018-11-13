81721c8aae0e8cc33b14ce73584963baecf93ad3
Daniel Roberts <BeardOfDan@gmail.com>
2018-11-13 09:34:46 -0800

    (feat) Complete "Generate Parentheses"

---
+++ 22. Generate Parentheses.js
@@ -0,0 +1,27 @@
+const backtrack = (result, current, open, close, max) => {
+  if (current.length === (max * 2)) {
+    result.push(current);
+    return;
+  }
+
+  if (open < max) {
+    backtrack(result, current + '(', open + 1, close, max);
+  }
+
+  if (close < open) {
+    backtrack(result, current + ')', open, close + 1, max)
+  }
+};
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+  const result = [];
+
+  backtrack(result, '', 0, 0, n);
+
+  return result;
+};
+
