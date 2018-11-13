42b7aa7d37fd069cb5078d92798f5d1850a39663
Daniel Roberts <BeardOfDan@gmail.com>
2018-11-13 07:53:52 -0800

    (feat) Upgrade some of the output

--- _test.js
+++ _test.js
@@ -6,16 +6,29 @@
 //    and expected is the expected return result of f
 module.exports = {
   runTests: (f, inputs, useSpacing = true, spacing = '\n') => {
+    let hasHadFailed = false;
+
     for (let i = 0; i < inputs.length; i++) {
       const args = inputs[i][0];
       const expected = inputs[i][1];
       const actual = f(...args);
       let comparison = actual === expected;
+      const stringActual = JSON.stringify(actual, '', 2);
+      const stringExpected = JSON.stringify(expected, '', 2);
+
       if (Array.isArray(expected) || (typeof expected === 'object')) {
-        comparison = JSON.stringify(actual) === JSON.stringify(expected);
+        comparison = stringActual === stringExpected;
       }
 
-      console.assert(comparison, `case: ${JSON.stringify(...args)}, ${JSON.stringify(expected)} \n  Expected: \n${JSON.stringify(expected)}\n  Recieved: \n${JSON.stringify(actual)}`);
+      if ((hasHadFailed) && (comparison === false) && (i < inputs.length - 1)) {
+        console.log('\n');
+      }
+
+      if (comparison === false) {
+        hasHadFailed = true;
+      }
+
+      console.assert(comparison, `case #${i}: ${JSON.stringify(args.map((arg) => { return JSON.stringify(arg); }), '', 2)}, ${stringExpected} \n  Expected: \n${stringExpected}\n  Recieved: \n${stringActual}`);
     }
 
     if (useSpacing) {
@@ -25,16 +38,29 @@ module.exports = {
     return true; // signifies that all assertions were run
   },
   runTestsSingleArg: (f, inputs, useSpacing = true, spacing = '\n') => {
+    let hasHadFailed = false;
+
     for (let i = 0; i < inputs.length; i++) {
       const arg = inputs[i][0];
       const expected = inputs[i][1];
       const actual = f(arg);
       let comparison = actual === expected;
+      const stringActual = JSON.stringify(actual, '', 2);
+      const stringExpected = JSON.stringify(expected, '', 2);
+
       if (Array.isArray(expected) || (typeof expected === 'object')) {
-        comparison = JSON.stringify(actual) === JSON.stringify(expected);
+        comparison = stringActual === stringExpected;
       }
 
-      console.assert(comparison, `case: ${JSON.stringify(...args)}, ${JSON.stringify(expected)} \n  Expected: \n${JSON.stringify(expected)}\n  Recieved: \n${JSON.stringify(actual)}`);
+      if ((hasHadFailed) && (comparison === false) && (i < inputs.length - 1)) {
+        console.log('\n');
+      }
+
+      if (comparison === false) {
+        hasHadFailed = true;
+      }
+
+      console.assert(comparison, `case #${i}: ${JSON.stringify(arg, '', 2)}, ${stringExpected} \n  Expected: \n${stringExpected}\n  Recieved: \n${stringActual}`);
     }
 
     if (useSpacing) {
