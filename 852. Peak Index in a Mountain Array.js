ac9d234f94094d480ce68d281dc7e6d7d54f02a9
Daniel Roberts <BeardOfDan@gmail.com>
2018-11-18 08:22:47 -0800

    (feat) Improve "Peak Index in a Mountain Array"
    By definition, the "mountain array" has a length of at least 3, so
    the zeroth element cannot be the peak (otherwise it'd be a ramp).

--- 852. Peak Index in a Mountain Array.js
+++ 852. Peak Index in a Mountain Array.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var peakIndexInMountainArray = function (arr) {
-  for (let i = 0; i < (arr.length - 1); i++) {
+  for (let i = 1; i < (arr.length - 1); i++) {
     if (arr[i] > arr[i + 1]) {
       return i;
     }
