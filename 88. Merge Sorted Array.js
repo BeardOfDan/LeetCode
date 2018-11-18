a1a3e89102bc334f43b72b71b8325488a42112a8
Daniel Roberts <BeardOfDan@gmail.com>
2018-11-17 22:42:11 -0800

    (feat) Complete "Merge Sorted Array"

---
+++ 88. Merge Sorted Array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  const endLength = m + n;
+
+  // fill empty nums1 elements with nums2 elements
+  for (let i = m; i < endLength; i++) {
+    nums1[i] = nums2[i - m];
+  }
+
+  nums1 = nums1.sort((a, b) => { return a - b; }); // mutate nums1
+
+  // nums1 is the answer, no need to return anything
+};
