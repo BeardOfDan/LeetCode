781b2f88a994d578b872be58c0bf5cce1fc30039
Daniel Roberts <BeardOfDan@gmail.com>
2018-11-19 00:51:49 -0800

    (feat) Complete "Insert into a Binary Search Tree"
    Runtime: 120 ms, faster than 99.63% of JavaScript online submissions for Insert into a Binary Search Tree.

---
+++ 701. Insert into a Binary Search Tree.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoBST = function (root, val) {
+  if (root.val < val) {
+    if (root.right === null) {
+      root.right = { val, right: null, left: null };
+    } else {
+      insertIntoBST(root.right, val);
+    }
+  } else { // root.val > val
+    if (root.left === null) {
+      root.left = { val, right: null, left: null };
+    } else {
+      insertIntoBST(root.left, val);
+    }
+  }
+
+  return root;
+};
