e02fedcaf5ded8c83d16e31137663cde33504522
Daniel Roberts <BeardOfDan@gmail.com>
2018-11-24 01:54:38 -0800

    (feat) Reduce runtime for "Reverse Words in a String"
    Now, it puts everything into an array of characters and performs
    the operations in place.

--- 151. Reverse Words in a String.js
+++ 151. Reverse Words in a String.js
@@ -3,55 +3,63 @@
  * @returns {string}
  */
 var reverseWords = function (str) {
-  const result = split(rev(str), ' ').reverse();
+  const charArray = [];
 
-  for (let i = 0; i < result.length; i++) {
-    result[i] = rev(result[i]);
+  // these two variables are indexes that skip starting and ending whitespace
+  let revStart = str.length - 1; // starting position for reverse order
+  let revEnd = 0; // ending position for reverse order
+
+  for (let i = revStart; ((str[i] === ' ') && (i > -1)); i--) {
+    revStart--;
   }
 
-  if (result.length > 1) {
-    return result.join(' ');
-  } else if (result.length === 1) {
-    return result[0];
-  } else {
-    return '';
+  for (let i = revEnd; ((str[i] === ' ') && (i < str.length)); i++) {
+    revEnd++;
   }
-};
+  revEnd--; // to remove the [or equal] comparison
 
-// helper function that reverses a string
-const rev = (str) => {
-  for (let i = 0; i < (str.length / 2); i++) {
-    const swapSpace = str[i];
-    str[i] = str[str.length - 1 - i];
-    str[str.length - 1 - i] = swapSpace;
+  // fill in str in reverse order
+  for (let i = revStart; i > revEnd; i--) {
+    if ((str[i] === ' ') && (str[i - 1] === ' ')) {
+      continue; // skip redundant spaces between words
+    }
+    charArray.push(str[i]);
   }
 
-  return str;
-};
-
-// a helper function that deviates from split by never creating an element that is the delimiter
-const split = (str, del) => {
-  const result = [];
-
+  // re-reverse the words
   let start = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === del) {
-      if (start !== i) {
-        result.push(str.slice(start, i));
+  for (let i = 0; i < charArray.length; i++) {
+    if ((charArray[i] === ' ') && (start !== i)) { // the span of a word is found
+      // reverse the word
+      const end = i; // non inclusive, since is the trailing space
+      const length = end - start;
+      for (let j = 0; j < (length / 2); j++) {
+        const swapSpace = charArray[start + j];
+        charArray[start + j] = charArray[end - 1 - j];
+        charArray[end - 1 - j] = swapSpace;
       }
-      start = i + 1;
+
+      start = i + 1; // the next word starts after the space
     }
   }
 
-  if ((start < str.length) && (str[start] !== ' ')) {
-    result.push(str.slice(start, str.length));
+  // re-reverse the last word (wasn't done in the loop)
+  const end = charArray.length;
+  const length = end - start;
+  for (let i = 0; i < (length / 2); i++) {
+    const swapSpace = charArray[start + i];
+    charArray[start + i] = charArray[end - 1 - i];
+    charArray[end - 1 - i] = swapSpace;
   }
 
-  return result;
+  return charArray.join('');
 };
 
 require('./_test').runTestsSingleArg(reverseWords, [
   [' ', ''],
-  ["the sky is blue", "blue is sky the"]
+  ["the sky is blue", "blue is sky the"],
+  ["the   sky   is   blue", "blue is sky the"],
+  ["   the sky is blue    ", "blue is sky the"],
+  ["     the      sky     is     blue         ", "blue is sky the"]
 ]);
 
