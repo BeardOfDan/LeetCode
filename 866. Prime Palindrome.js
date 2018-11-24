77c8ec9257ee1b6082c07d5fdfa2c4c9fdaa2ead
Daniel Roberts <BeardOfDan@gmail.com>
2018-11-24 05:59:45 -0800

    (refactor) Various minor changes

--- 866. Prime Palindrome.js
+++ 866. Prime Palindrome.js
@@ -1,110 +1,14 @@
-// /**
-//  * @param {number} N
-//  * @return {number}
-//  */
-// var primePalindrome = function (N) {
-//   const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
-
-//   let index = 0; // index of first prime not less than N
-
-//   if (primes[primes.length - 1] < N) {
-//     index = primes.length - 1; // skip to end of the primes array
-
-//     // find more primes until at least of size N
-//     while (primes[primes.length - 1] < N) {
-//       getAnotherPrime(primes);
-//       index++;
-//     }
-//   }
-
-//   if (index === 0) { // if index was not previously updated
-//     for (let i = 0; i < primes.length; i++) {
-//       if (primes[index] < N) {
-//         index++;
-//         continue;
-//       }
-//       break;
-//     }
-//   }
-
-//   // test if is palindrome
-//   for (; index < primes.length; index++) {
-//     if (numIsPalindrome(primes[index])) {
-//       return primes[index];
-//     }
-//   }
-
-//   // while no palindrome primes, not less than N, find another prime
-//   while (true) {
-//     const prime = getAnotherPrime(primes);
-//     if (numIsPalindrome(prime)) {
-//       return prime;
-//     }
-//   }
-// };
-
-// // mutates the primes array
-// const getAnotherPrime = (primes) => {
-//   let index = primes[primes.length - 1] + 2; // check the next odd number
-
-//   while (true) {
-//     let isPrime = true;
-//     const root = Math.sqrt(index) + 1; // plus 1 to remove [or equal] comparison
-//     for (let i = 0; primes[i] < root; i++) {
-//       if ((index % primes[i]) === 0) {
-//         isPrime = false;
-//         index += 2; // check the next odd number
-//         break;
-//       }
-//     }
-
-//     if (isPrime) {
-//       primes.push(index); // returns the new prime
-//       return index;
-//     }
-//   }
-// };
-
-// const numIsPalindrome = (num) => {
-//   const str = num.toString();
-
-//   for (let i = 0; i < (str.length / 2); i++) {
-//     if (str[i] !== str[str.length - 1 - i]) {
-//       return false;
-//     }
-//   }
-
-//   return true;
-// };
-
-// require('./_test').runTestsSingleArg(primePalindrome, [
-//   [6, 7],
-//   [8, 11],
-//   [13, 101],
-//   [1215122, 1218121],
-//   [9932400, 9935399]
-// ]);
-
-// console.log(primePalindrome(9932400));
-
-
-
-
-
-
-
-
 /**
  * @param {number} N
  * @return {number}
  */
 var primePalindrome = function (N) {
-  for (let L = 1; L <= 5; ++L) {
+  for (let L = 1; L <= 5; L++) {
     // Check for odd-length palindromes
     for (let root = Math.pow(10, L - 1); root < Math.pow(10, L); root++) {
       let str = root.toString();
-      for (k = L - 2; k >= 0; k--) {
-        str += str[k];
+      for (let i = L - 2; i >= 0; i--) {
+        str += str[i];
       }
 
       const palindrome = parseInt(str);
@@ -116,8 +20,8 @@ var primePalindrome = function (N) {
     // Check for even-length palindromes
     for (let root = Math.pow(10, L - 1); root < Math.pow(10, L); root++) {
       let str = root.toString();
-      for (k = L - 1; k >= 0; k--) {
-        str += str[k];
+      for (let i = L - 1; i >= 0; i--) {
+        str += str[i];
       }
 
       const palindrome = parseInt(str);
@@ -131,9 +35,9 @@ var primePalindrome = function (N) {
 const isPrime = (N) => {
   if (N < 2) {
     return false;
-  } else if (N === 2) {
+  } else if (N === 2) { // handle only even prime number
     return true;
-  } else if ((N % 2) === 0) {
+  } else if ((N % 2) === 0) { // handle all even numbers in constant, rather than linear, time
     return false;
   }
 
